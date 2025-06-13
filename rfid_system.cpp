1fdbbaa188fa952d96fe1750e90e3222584b1200
Your GitHub Username <your@email.com>
2025-06-13 22:04:57 +0500

    second day

--- rfid_system.cpp
+++ rfid_system.cpp
@@ -7,3 +7,11 @@
 #define RST_PIN 9
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+Servo lockServo;
+
+byte authorizedUID[] = { 0xDE, 0xAD, 0xBE, 0xEF }; Example UID
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
